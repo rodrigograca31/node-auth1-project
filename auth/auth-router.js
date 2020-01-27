bbefcb73045330aa14d3e2544b841f0b29698619
rodrigograca31 <rodrigograca1993@gmail.com>
2020-01-27 19:56:51 +0000

    Finish

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -6,7 +6,9 @@ const uuid = require("uuid");
 const activeSessions = [];
 
 router.post("/register", (req, res) => {
-	Users.add(user)
+	req.body.password = bcrypt.hashSync(req.body.password, 10);
+
+	Users.add({ username: req.body.username, password: req.body.password })
 		.then(saved => {
 			res.status(201).json(saved);
 		})
@@ -16,15 +18,34 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-	Users.findBy({ username })
+	Users.findBy({ username: req.body.username })
 		.first()
-		.then(user => {})
+		.then(user => {
+			console.log("found", user);
+			if (user && bcrypt.compareSync(req.body.password, user.password)) {
+				const sessionId = uuid();
+				activeSessions.push(sessionId);
+
+				res.cookie("sessionId", sessionId, { maxAge: 900000 });
+				res.status(200).json({ message: "Welcome" });
+			} else {
+				res.status(200).json({
+					message: "Username not found or wrong password"
+				});
+			}
+		})
 		.catch(error => {
-			res.status(500).json(error);
+			res.status(500).json({ error: error.message });
 		});
 });
 
-function protected(req, res, next) {}
+function protected(req, res, next) {
+	if (activeSessions.includes(req.cookies.sessionId)) {
+		next();
+	} else {
+		res.status(401).json({ message: "Not valid" });
+	}
+}
 
 router.protected = protected;
 
